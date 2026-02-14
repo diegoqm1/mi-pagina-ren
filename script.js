17996c320e4bf2e87182bb071ce2a55696b6583b
diegoqm1 <quinta.mondra@gmail.com>
2026-02-13 23:43:00 -0600

    Update and rename script.js.txt to script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+body {
+font-family: Arial;
+text-align: center;
+background: #ffe6f0;
+color: #333;
+}
+
+h1, h2 {
+color: #ff66a3;
+}
+
+#contador {
+font-size: 24px;
+margin: 20px;
+background: white;
+padding: 10px;
+border-radius: 15px;
+animation: latido 2s infinite;
+}
+
+@keyframes latido {
+0% { transform: scale(1); }
+50% { transform: scale(1.05); }
+100% { transform: scale(1); }
+}
+
+.galeria {
+display: grid;
+grid-template-columns: repeat(5, 1fr);
+gap: 15px;
+padding: 20px;
+}
+
+.foto img {
+width: 100%;
+border-radius: 15px;
+cursor: pointer;
+transition: transform 0.3s;
+}
+
+.foto img:hover {
+transform: scale(1.1);
+}
+
+#mensaje {
+margin-top: 20px;
+font-size: 18px;
+background: white;
+padding: 15px;
+border-radius: 15px;
+opacity: 0;
+transition: opacity 0.5s;
+}
